7fd88192dca9d9d300b8ceba4b4d56588a0fd049
Pedro Lucas <pedrolucasalmeidanogueira@gmail.com>
2025-12-28 18:45:47 -0300

    learning templates

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'home.html')
 
 def contato(request):
     return HttpResponse('contato')
