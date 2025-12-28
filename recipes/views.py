2802158dda928b440b639650c46143a6617ffe2e
Pedro Lucas <pedrolucasalmeidanogueira@gmail.com>
2025-12-28 19:12:06 -0300

    learning render

--- recipes/views.py
+++ recipes/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html',context={
+        'name':'Pedro'
+    },status=201)
 
 def contato(request):
     return HttpResponse('contato')
